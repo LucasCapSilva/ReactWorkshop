22695ef05b30fefc0b0cbb8cd198ca708972eadf
unknown <lcapelotto@gmail.com>
2022-06-14 14:27:39 -0300

    1.0

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ const App = () => {
   return (
     <div>
       <NavBar></NavBar>
-    <Produtos></Produtos>
+      <Produtos></Produtos>
 
       <Footer></Footer>
     </div>
--- src/paginas/produtos/Produtos.tsx
+++ src/paginas/produtos/Produtos.tsx
@@ -1,4 +1,4 @@
-import { Box, Grid, Switch, Typography } from '@mui/material'
+import { Box, Grid, Switch, TextField, Typography } from '@mui/material'
 import CardProdutos from '../../components/cards/cardsProduto/CardProdutos'
 
 const Produtos = () => {
@@ -23,53 +23,71 @@ const Produtos = () => {
     <>
       <Box sx={{ ...styleBackgroud }}>
         <Grid container >
-          <Grid item xs={3} >
+        <Grid item xs={3} >
+                    <Box margin={2}  marginLeft={9}>
+                        <Typography variant="body1" textAlign="start" color="initial">
+                            Instrumentos Musicais &gt; Instrumentos de Corda</Typography>
+                    </Box>
+                    <Box margin={2} marginLeft={9}>
+                        <Typography variant="h5" style={{ fontWeight: "bold" }} textAlign="start" color="initial">
+                            Violoncelo</Typography>
+                    </Box>
+                    <Box display="flex" justifyContent="center">
+                        <Box sx={{ ...commonStyles, borderRadius: '5px',borderColor:'#cecece', padding: 1, display: "flex", justifyContent: "space-between", alignItems: "center" }} >
+                            <Typography variant="body1" style={{ fontWeight: "bold" }} textAlign="start" color="initial">
+                                Chegam amanhã</Typography>
+                            <Switch />
+                        </Box>
 
-            <Box margin={2} marginLeft={9}>
-              <Typography variant="body1" textAlign="start" color="initial">
-                Instrumentos Musicais &gt; Instrumentos de Corda
-              </Typography>
-            </Box>
-            <Box margin={2} marginLeft={9}>
-              <Typography variant="h5" style={{ fontWeight: "bold" }} textAlign="start" color="initial">
-                Violoncelo</Typography>
-            </Box>
+                    </Box>
+                    <Box display="flex" justifyContent="center">
+                        <Box sx={{ ...commonStyles, borderRadius: '5px',borderColor:'#cecece', padding: 1, display: "flex", justifyContent: "space-between", alignItems: "center" }} >
+                            <Typography variant="body1" style={{ fontWeight: "bold" }} textAlign="start" color="initial">
+                                Frete grátis</Typography>
+                            <Switch />
+                        </Box>
 
-            <Box display="flex" justifyContent="center">
-              <Box sx={{ ...commonStyles, borderRadius: '5px', borderColor: '#cecece', padding: 1, display: "flex", justifyContent: "space-between", alignItems: "center" }} >
-                <Typography variant="body1" style={{ fontWeight: "bold" }} textAlign="start" color="initial">
-                  Chegam amanhã</Typography>
-                <Switch />
-              </Box>
+                    </Box>
 
-            </Box>
+                    <Box marginY={3} marginLeft={9}>
+                        <Typography variant="h6" style={{ fontWeight: "bold" }} textAlign="start" color="initial">
+                            Condição</Typography>
+                        <Typography variant="body2"  textAlign="start" color="initial">
+                            Novo
+                            (393)</Typography>
+                        <Typography variant="body2" textAlign="start" color="initial">
+                            Usado
+                            (4)</Typography>
 
-            <Box display="flex" justifyContent="center">
-              <Box sx={{ ...commonStyles, borderRadius: '5px', borderColor: '#cecece', padding: 1, display: "flex", justifyContent: "space-between", alignItems: "center" }} >
-                <Typography variant="body1" style={{ fontWeight: "bold" }} textAlign="start" color="initial">
-                  Frete grátis</Typography>
-                <Switch />
-              </Box>
+                    </Box>
 
-            </Box>
+                    <Box marginY={3} marginLeft={9}>
+                        <Typography variant="h6" style={{ fontWeight: "bold" }} textAlign="start" color="initial">
+                            Preço</Typography>
+                        <Typography variant="body2" textAlign="start" color="initial">
+                            Até R$350</Typography>
+                        <Typography variant="body2" textAlign="start" color="initial">
+                            R$350 a R$3.500</Typography>
+                        <Typography variant="body2" textAlign="start" color="initial">
+                            Mais de R$3.500 (134)</Typography>
+                        <Box display="flex" justifyContent="start" marginY={2} marginRight={9}>
 
-            <Box marginY={3} marginLeft={9}>
-              <Typography variant="h6" style={{ fontWeight: "bold" }} textAlign="start" color="initial">
-                Condição</Typography>
-              <Typography variant="body2" textAlign="start" color="initial">
-                Novo (4)</Typography>
-              <Typography variant="body2" textAlign="start" color="initial">
-                Usado
-                (4)</Typography>
-
-            </Box>
-
-            <Box marginY={3} marginLeft={9}>
+                            <TextField style={{backgroundColor:'#FFFFFF'}} id="outlined-basic" size="small" label="Mínimo" variant="outlined" /> _ <TextField style={{backgroundColor:'#FFFFFF'}} id="outlined-basic" size="small" label="Máximo" variant="outlined" />
+                        </Box>
+                    </Box>
 
 
-            </Box>
 
-          </Grid>
+                    <Box marginY={3} marginLeft={9}>
+                        <Typography style={{ fontWeight: "bold" }} variant="h6" textAlign="start" color="initial">
+                            Tempo de entrega</Typography>
+                        <Typography variant="body2" textAlign="start" color="initial">
+                            Chegará em menos de 24h (6)</Typography>
+
+
+                    </Box>
+
+                </Grid>
           <Grid item xs={9}>
 
             <CardProdutos />
